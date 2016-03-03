54d4cae968aa7bdb8986b87bbbf7d569ba4c1628
QiaozhiWang <wangqiaozhi@126.com>
2016-03-03 12:39:16 -0600

    Stemmed file saved as .pickle; tf_idf created (Maybe slow)

--- sl-stem.py
+++ sl-stem.py
@@ -1,4 +1,5 @@
 import pickle
+import os
 from nltk.corpus import stopwords, wordnet
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import PorterStemmer, WordNetLemmatizer
@@ -20,12 +21,12 @@ def get_wordnet_pos(treebank_tag):		#seperate to different kind of word
 		return ''
 
 #-------------------open text processed by Young-------------------------#
-#filename = os.listdir("Documents")		#get all the filenames of 63 documents under folder Documents
-#f = open(str('filename[i].pickle'),'rb')
+#filenames = os.listdir("raw_Documents")		#get all the filenames of 63 documents under folder Documents
+#f = open(str('filenames[i].pickle'),'rb')
 #text = pickle.load(f)
 
 #----------------------example-----------------------------------#
-text = "Better Machine learning is the science of getting computers to act without being explicitly programmed. In the past decade, machine learning has given us self-driving cars, practical speech recognition, effective web search, and a vastly improved understanding of the human genome. Machine learning is so pervasive today that you probably use it dozens of times a day without knowing it. Many researchers also think it is the best way to make progress towards human-level AI. In this class, you will learn about the most effective machine learning techniques, and gain practice implementing them and getting them to work for yourself. More importantly, you'll learn about not only the theoretical underpinnings of learning, but also gain the practical know-how needed to quickly and powerfully apply these techniques to new problems. Finally, you'll learn about some of Silicon Valley's best practices in innovation as it pertains to machine learning and AI."
+text = "Yellowstone National Park (Arapaho: Henihco'oo or Héetíhco'oo)[4] is a national park located primarily in the U.S. state of Wyoming, although it also extends into Montana and Idaho. It was established by the U.S. Congress and signed into law by President Ulysses S. Grant on March 1, 1872.[5][6] Yellowstone, the first National Park in the U.S. and widely held to be the first national park in the world,[7] is known for its wildlife and its many geothermal features, especially Old Faithful Geyser, one of the most popular features in the park.[8] It has many types of ecosystems, but the subalpine forest is the most abundant. It is part of the South Central Rockies forests ecoregion."
 tokens = word_tokenize(text)
 words = [w.lower() for w in tokens if w.isalpha()]
 
@@ -35,10 +36,14 @@ filtered_text = [w for w in words if not w in stop_words]
 tagged_tokens = pos_tag(filtered_text)
 
 #Lemmatize and Stemmer (I'm afraid lemmatizer improve recall, but decrease precision)
+document = []
 for i in range(0,len(filtered_text)):
 	if get_wordnet_pos(tagged_tokens[i][1]):
 		w = lemmatizer.lemmatize(filtered_text[i], get_wordnet_pos(tagged_tokens[i][1]))
 		w = ps.stem(w)
 	else:
 		w = ps.stem(filtered_text[i])
+	document.append(w)
 	print(w)
+	pickle.dump(document,open('document4.pickle','wb'))
+
