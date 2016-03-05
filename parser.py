3d8fd8b8b5240567bed58827d5470c506f7f3588
youngliu323 <youngliu323@gmail.com>
2016-03-05 13:12:19 -0600

    now writes to a text file and delimites by space

--- parser.py
+++ parser.py
@@ -1,15 +1,21 @@
 import re
 from HTMLParser import HTMLParser
 
+
 f = open('Arches_National_Park.htm','r')
 data = f.read()
 #print data
 
-#the big list that I pass on to the next guy
-l = []
+n = open("a.txt",'w')
+
+def letters(input):
+    return ''.join([c for c in input if str.isalpha or c in str.isspace])
+    #return ''.join(filter(str.isalpha, input))
+    #return ''.join([c for c in input if c in str.isalpha or c in str.isspace])
 
 def parseStringBySpace(string):
-    	string.split(" ")
+    	string = string.split(" ")
+    	print string
     	return string
 
 # create a subclass and override the handler methods
@@ -17,23 +23,15 @@ class MyHTMLParser(HTMLParser):
 
     def handle_data(self, data):
         #print data
-        data = data.replace(',','')
-        data = data.replace(';','')
-        data = data.replace(':','')
+        #data = data.replace(',','')
+        data = data.replace('\n','')
+        data = data.replace('\t',' ')
         data = data.lower()
+        data = letters(data)
         print data
-        #l.append(parseStringBySpace(data))
+        n.write(data)
+
 
 # instantiate the parser and fed it some HTML
 parser = MyHTMLParser()
 parser.feed(data)
-
-
-a = "AAAA"
-print a
-print a.lower()
-
-#print l
-
-#'<html><head><title>Test</title></head>'
- #           '<body><h1>Parse me!</h1></body></html>'
