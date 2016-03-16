b7b21cd834700b46354548fdaeddac48b56d7973
youngliu323 <youngliu323@gmail.com>
2016-03-16 16:50:14 -0500

    Cleaned up more and pickle files should be right now

--- docsnew/parser.py
+++ docsnew/parser.py
@@ -1,18 +1,10 @@
 import re
 import os
+import pickle
 from HTMLParser import HTMLParser
 
+#creates the global variable.. pretty bad programming
 n = open("a.txt",'w')
-
-def letters(input):
-    return ''.join([c for c in input if str.isalpha or c in str.isspace])
-    #return ''.join(filter(str.isalpha, input))
-    #return ''.join([c for c in input if c in str.isalpha or c in str.isspace])
-
-def parseStringBySpace(string):
-    	string = string.split(" ")
-    	print string
-    	return string
 
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
@@ -22,19 +14,17 @@ class MyHTMLParser(HTMLParser):
         data = data.replace('  ', ' ')
         data = data.replace('\t',' ')
         data = data.lower()
-        data = letters(data)
-        if "document." in data:
-            data = ""
-        if "{{{" in data:
+        #data = letters(data)
+        if "document." in data: #filters out bad stuff
             data = ""
         if "\",\"" in data:
+            #print data For testing purposes
             data = ""
         if "\"})" in data:
             data = ""
         if re.match('\w', data):
             data = data + " "
             n.write(data)
-
 
 # List and open every file in current dir
 html_files = [file for file in os.listdir(".") if file.endswith(".htm") or file.endswith(".html")]
@@ -43,18 +33,25 @@ html_files = [file for file in os.listdir(".") if file.endswith(".htm") or file.
     # y = file name
 for x,y in enumerate(html_files):
     with open(y, 'r') as fin:
+
+        #certain image files that we don't really care for...
         data = fin.read().decode('ascii','ignore')
 
+        #we first strip away the .htm part of the naming
         z =  y[:-4]
-        n = open(z + ".txt",'w')
+        n = open(z + ".txt",'w') #then we concatenate the rest
         parser = MyHTMLParser()
         parser.feed(data)
 
-#print data
+#temporary list to store each word
+file_list = []
 
-    #write the files
+txtFiles = [file for file in os.listdir(".") if file.endswith(".txt")]
 
-
-    # instantiate the parser and fed it some HTML
-    #parser = MyHTMLParser()
-    #parser.feed(data)
+for x,y in enumerate(txtFiles):
+    with open(y, 'r') as f:
+        for eachLine in f: #strips out the new lines from the file
+            file_list.append(eachLine.strip())
+        z =  y[:-4]
+        n = open(z + ".pickle",'w')
+        pickle.dump(file_list,n)
