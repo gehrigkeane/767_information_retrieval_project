799e9a6957243be59558615ce2a4974de4860964
QiaozhiWang <wangqiaozhi@126.com>
2016-04-15 20:38:38 -0500

    Fixed same document_vectors

--- tf-idf.py
+++ tf-idf.py
@@ -4,6 +4,7 @@ from collections import Counter
 import os, csv, sys
 import math
 import llist
+import pprint as pp
 
 sys.setrecursionlimit(5000)
 
@@ -22,9 +23,12 @@ while True:
 		inverted_index = pickle.load(f)	
 	except EOFError:
 		break
-
+#print("inverted_index: ")
+#print(inverted_index)
 for key, value in inverted_index.items():
 	total_idf.update({key:math.log(N/value[0],10)})
+#print("total_idf")
+#print(total_idf)
 
 o_path = "new_processed"
 if not os.path.exists(o_path): 	
@@ -43,28 +47,31 @@ while True:
 		document_index = pickle.load(f)	
 	except EOFError:
 		break
+#print("document_index:")
+#pp.pprint(document_index)
 i = 0
-for key, value in document_index.items():
+for key, value in document_index.items(): 
 	#document_list.update({i:key})
-	#pickle.dump(document_list,open(str(o_path)+"/document_list.pickle",'wb'))
+	#pickle.dump(document_list,open(str(o_path)+"/document_list.pickle",'wb')) 
 	with open(str(o_path)+"/document_list.csv",'a',newline='',encoding='utf8') as f_docindex:
 		writer1 = csv.writer(f_docindex)
 		writer1.writerow([i, key])
 	i += 1
 	D = []
-	for term, tf in value.items():
+	for term, tf in value.items():            
 		if term in total_idf:
-			D.append(value[term]*total_idf[term])
+			D.append(tf*total_idf[term])
 		else:
 			D.append('0')
 	D_vectors.append(D)
+
 with open(str(o_path)+"/document_vectors.csv",'a',newline='',encoding='utf8') as f_docvec:
 	writer2 = csv.writer(f_docvec)
 	for pD in D_vectors:
-		writer2.writerow(D)
+		writer2.writerow(pD)
 	#pickle.dump(D_vectors,open(str(o_path)+"/document_vectors.pickle",'wb'))
-
-
+pp.pprint("D_vectors")
+pp.pprint(D_vectors)
 def similarity(D1,D2):
 	v_sum = 0
 	s1_sum = 0
