c8bdf789edb7524b0b2d325d52c12074fedaff7a
youngliu323 <youngliu323@gmail.com>
2016-04-17 20:52:16 -0500

    deleted unnecessary comments

--- UserQuery/queryVector.rb
+++ UserQuery/queryVector.rb
@@ -7,15 +7,6 @@ load 'userQueryParser.rb'
 #key is the term, value is the list of 3 values,
 #in addition to a posting list
 
-#we would take our query that's been stemmed
-#for each document
-#for i in 0 ... userQuery.size
-#  if simpleHash.has_key?(userQuery[i])
-  #  puts "it's true bro"
-    #we would put the idfs here
-#  end
-#end
-
 
 def queryHasher(userQuery)
   userHash = {}
