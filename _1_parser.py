b7b7681345735b57009d73f199a5373de5e113d3
youngliu323 <youngliu@rocketmail.com>
2016-04-30 22:46:41 -0500

    now it will create title and snapshots files

--- _1_parser.py
+++ _1_parser.py
@@ -43,6 +43,46 @@ def parse_html(pathname, test):
 			if not os.path.exists(o_pathname):
 				os.makedirs(o_pathname)
 			open(o_pathname + filename,'w').write(parser.data)
+#i made title and snap csv files as arguments in case you wanted to test and change stuff
+#titlecsv-title of the csv file you want to write to and same with snapcsv
+#also, this function adds to a file
+def createTitleAndSnaps(pathname, test, titlecsv, snapcsv):
+	html_files = [file for file in os.listdir("Plotsummary/") if file.endswith(".htm") or file.endswith(".html")]
+    	o_pathname = "3.ASSETS/"
+    	# x = file number 0, 1, ..., n
+    	# y = file name
+	open("titleCSV/titleCollections.csv",'w').write("")
+	open("snapShotCSV/snapShotCollection.csv",'w').write("")
+	for x,y in enumerate(html_files):
+		if test and y != '0033110.html':			# Isolates document pool for testing
+				continue
+		ofile = open(pathname+y)
+	    	soup = bsoup(ofile)
+		#-------------------------------------------------#
+		#finds the film TITLE and write them to a csv file
+		#-------------------------------------------------#
+		b = soup.find(itemprop="name").find('a', href=True)
+		title = '\'' + b.get_text() + '\''
+		filename = y[:-5]+","
+	 	with open(o_pathname+titlecsv, "a") as myfile:
+	        	myfile.write(filename)
+	        	#some titles have weird characters in them
+	        	myfile.write(title.encode('utf8') + ',' + '\n')
+	
+	    	#-------------------------------------------------#
+	    	#finds the film PLOTSUMMARY and write them to a csv file
+	    	#-------------------------------------------------#
+	    	desc = soup.find("p", class_="plotSummary")
+	    	filename = y[:-5]+","
+	    	with open(o_pathname+snapcsv, "a") as myfile:
+	        	myfile.write(filename)
+	    	if desc != None:
+		        snap = '\'' + desc.get_text().replace('\n','') + '\''
+		        with open(o_pathname+snapcsv, "a") as myfile:
+		        	myfile.write(snap.encode('utf8')+',\n')
+		else:
+			with open(o_pathname+snapcsv, "a") as myfile:
+				myfile.write('\'\','+'\n')
 
 #------------------------------------------------------------------------------------------
 #	Correct invokation of parse_html
